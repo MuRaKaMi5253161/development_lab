dce3d347dfe6f136a4a20c8e93df8123f0ab043c
MuRaKaMi5253161 <taitai7121@gmail.com>
2023-12-10 22:24:55 +0900

    loomsページ作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,16 @@ import './css/Header.css';
 // ui
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Header: React.FC = () => {
     return (
         <div className="header">
             <div className="header-menu-line">
+            <span className="icon x">
+                    <a href='/' className='homeIcon'><HomeIcon className='Icon home' /></a>
+                </span>
+
                 <span className="icon x">
                     <a href='https://twitter.com/unko____ta' target="_blank" rel="noopener noreferrer" className='xIcon'><TwitterIcon className='Icon Twitter' /></a>
                 </span>
--- src/components/pages/Looms.tsx
+++ src/components/pages/Looms.tsx
@@ -1,16 +1,59 @@
 import React from 'react';
-import { useLocation  } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+
+// css
+import '../css/pages.css'
+
+// skilsIcon
+import htmlIcon from '../svg/html-5.svg';
+import cssIcon from '../svg/css-3.svg';
+import jsIcon from '../svg/javascript.svg';
+import reactIcon from '../svg/react.svg';
+import firebaseIcon from '../svg/firebase.svg';
+import muiIcon from '../svg/material-ui.svg';
+
+// appImage
+import loomsImg1 from '../img/loomsImage1.png';
+import loomsImg2 from '../img/loomsImage2.png';
+import loomsImg3 from '../img/loomsImage3.png';
 
 const Looms: React.FC = () => {
 
-    // const location = useLocation();
+    const location = useLocation();
 
-    // const appName = location.state.appName;
-    // const createDate = location.state.date;
+    const appName = location.state.appName;
 
     return(
-        <div>
-            <p>なぜエラーが出るん</p>
+        <div className='Looms'>
+            <div className='appProfile'>
+                <p className='appName'>{appName}</p>
+
+                <p className='appUrl'>-URL-</p>
+                <a href='https://looms-831f0.web.app' target='_blank' rel='noopener noreferrer' className='applink'>https://looms-831f0.web.app</a>
+
+                <div>
+                    <p className='appskil'>-skil-</p>
+                    <span><img className='skilIcons' src={htmlIcon} alt='skilIcon' /></span>
+                    <span><img className='skilIcons' src={cssIcon} alt='skilIcon' /></span>
+                    <span><img className='skilIcons' src={jsIcon} alt='skilIcon' /></span>
+                    <span><img className='skilIcons' src={reactIcon} alt='skilIcon' /></span>
+                    <span><img className='skilIcons' src={firebaseIcon} alt='skilIcon' /></span>
+                    <span><img className='skilIcons' src={muiIcon} alt='skilIcon' /></span>
+                </div> 
+
+                <div>
+                    <p className='appImageText'>-Image-</p>
+                    <div>
+                        <img className='appImage' src={loomsImg1}  alt="appImage" />
+                    </div>
+                    <div>
+                        <img className='appImage' src={loomsImg2}  alt="appImage" />
+                    </div>
+                    <div>
+                        <img className='appImage' src={loomsImg3}  alt="appImage" />
+                    </div>
+                </div>    
+            </div>
         </div>
     );
 };
